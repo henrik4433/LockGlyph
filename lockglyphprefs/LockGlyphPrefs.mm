e3cce9035d04ce49e01ceebb0b45f1b0b7d6f78d
Evan Petousis <evilgoldfish01@me.com>
2014-12-30 23:25:54 +1100

    Changed the look of the preferences

--- lockglyphprefs/LockGlyphPrefs.mm
+++ lockglyphprefs/LockGlyphPrefs.mm
@@ -49,6 +49,96 @@
 		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"https://mobile.twitter.com/" stringByAppendingString:user]]];
 
 }
+@end
+
+@interface LockGlyphTitleCell : PSTableCell <PreferencesTableCustomView> {
+	UILabel *tweakTitle;
+	UILabel *tweakSubtitle;
+}
+
+@end
+
+@implementation LockGlyphTitleCell
+
+- (id)initWithSpecifier:(PSSpecifier *)specifier
+{
+	self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+
+	if (self) {
+
+		int width = [[UIScreen mainScreen] bounds].size.width;
+
+		CGRect frame = CGRectMake(0, 20, width, 60);
+		CGRect subtitleFrame = CGRectMake(0, 55, width, 60);
+
+		tweakTitle = [[UILabel alloc] initWithFrame:frame];
+		[tweakTitle setNumberOfLines:1];
+		[tweakTitle setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:48]];
+		[tweakTitle setText:@"LockGlyph"];
+		[tweakTitle setBackgroundColor:[UIColor clearColor]];
+		[tweakTitle setTextColor:[UIColor blackColor]];
+		[tweakTitle setTextAlignment:NSTextAlignmentCenter];
+
+		tweakSubtitle = [[UILabel alloc] initWithFrame:subtitleFrame];
+		[tweakSubtitle setNumberOfLines:1];
+		[tweakSubtitle setFont:[UIFont fontWithName:@"HelveticaNeue-Regular" size:18]];
+		[tweakSubtitle setText:@"By evilgoldfish."];
+		[tweakSubtitle setBackgroundColor:[UIColor clearColor]];
+		[tweakSubtitle setTextColor:[UIColor colorWithRed:119/255.0f green:119/255.0f blue:122/255.0f alpha:1.0f]];
+		[tweakSubtitle setTextAlignment:NSTextAlignmentCenter];
+
+		[self addSubview:tweakTitle];
+		[self addSubview:tweakSubtitle];
+	}
+
+	return self;
+}
+
+- (CGFloat)preferredHeightForWidth:(CGFloat)arg1{
+    return 125.0f;
+}
+
+@end
+
+// Other preference panels
+
+@interface LockGlyphBehaviourPrefsListController: PSListController {
+}
+@end
+
+@implementation LockGlyphBehaviourPrefsListController
+- (id)specifiers {
+	if(_specifiers == nil) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"LockGlyphPrefs-Behaviour" target:self] retain];
+	}
+	return _specifiers;
+}
+@end
+
+@interface LockGlyphAnimationsPrefsListController: PSListController {
+}
+@end
+
+@implementation LockGlyphAnimationsPrefsListController
+- (id)specifiers {
+	if(_specifiers == nil) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"LockGlyphPrefs-Animations" target:self] retain];
+	}
+	return _specifiers;
+}
+@end
+
+@interface LockGlyphAppearancePrefsListController: PSListController {
+}
+@end
+
+@implementation LockGlyphAppearancePrefsListController
+- (id)specifiers {
+	if(_specifiers == nil) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"LockGlyphPrefs-Appearance" target:self] retain];
+	}
+	return _specifiers;
+}
 
 -(void)resetColors {
 	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Reset colours"
@@ -109,51 +199,6 @@
 
     return files;
 }
-@end
-
-@interface LockGlyphTitleCell : PSTableCell <PreferencesTableCustomView> {
-	UILabel *tweakTitle;
-	UILabel *tweakSubtitle;
-}
-
-@end
-
-@implementation LockGlyphTitleCell
-
-- (id)initWithSpecifier:(PSSpecifier *)specifier
-{
-	self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
-
-	if (self) {
-
-		int width = [[UIScreen mainScreen] bounds].size.width;
-
-		CGRect frame = CGRectMake(0, -30, width, 60);
-		CGRect subtitleFrame = CGRectMake(0, 5, width, 60);
-
-		tweakTitle = [[UILabel alloc] initWithFrame:frame];
-		[tweakTitle setNumberOfLines:1];
-		[tweakTitle setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:48]];
-		[tweakTitle setText:@"LockGlyph"];
-		[tweakTitle setBackgroundColor:[UIColor clearColor]];
-		[tweakTitle setTextColor:[UIColor blackColor]];
-		[tweakTitle setTextAlignment:NSTextAlignmentCenter];
-
-		tweakSubtitle = [[UILabel alloc] initWithFrame:subtitleFrame];
-		[tweakSubtitle setNumberOfLines:1];
-		[tweakSubtitle setFont:[UIFont fontWithName:@"HelveticaNeue-UltraLight" size:20]];
-		[tweakSubtitle setText:@"By evilgoldfish."];
-		[tweakSubtitle setBackgroundColor:[UIColor clearColor]];
-		[tweakSubtitle setTextColor:[UIColor blackColor]];
-		[tweakSubtitle setTextAlignment:NSTextAlignmentCenter];
-
-		[self addSubview:tweakTitle];
-		[self addSubview:tweakSubtitle];
-	}
-
-	return self;
-}
-
 @end
 
 // vim:ft=objc
